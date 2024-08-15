f42b704ca1858191b759032cb41be7a5065d4909
Iury da Silva <iury.silva@weduu.com>
2024-08-15 20:30:59 -0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<script setup>
+import { useCounterStore } from './stores/counter'
+import { computed } from 'vue'
+import Button from 'primevue/button'
+
+const counterStore = useCounterStore()
+const increment = () => {
+  counterStore.increment()
+}
+
+const decrement = () => {
+  counterStore.decrement()
+}
+const counter = computed(() => counterStore.count)
+</script>
+
+<template>
+  <div>
+    <Button @click="increment">Increment</Button>
+    <Button @click="decrement">Decrement</Button>
+    <p>Counter: {{ counter }}</p>
+    <RouterView />
+  </div>
+</template>
+
+<style scoped></style>
---
+++ src/main.js
@@ -0,0 +1,21 @@
+import './assets/main.css'
+
+import { createApp } from 'vue'
+import { createPinia } from 'pinia'
+import PrimeVue from 'primevue/config'
+import Aura from '@primevue/themes/aura'
+
+import App from './App.vue'
+import router from './router'
+
+const app = createApp(App)
+
+app.use(createPinia())
+app.use(router)
+app.use(PrimeVue, {
+  theme: {
+    preset: Aura  
+  }
+})
+
+app.mount('#app')
