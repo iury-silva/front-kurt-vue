a2458be9a15d5b998986071727e2044ad049519d
Helena Della Giustina <haguiar@imply.com>
2024-09-27 20:20:28 -0300

    refactor: Separating menu component in layout

---
+++ src/Layouts/Default.vue
@@ -0,0 +1,19 @@
+<template>
+    <main class="flex">
+      <Menu />
+      <section class="main w-full">
+        <slot></slot>
+      </section>
+    </main>
+  </template>
+  
+  <script setup>
+  import Menu from '@/components/Menu.vue';
+  
+  </script>
+  
+  <style lang="scss" scoped> 
+  .main {
+    background: theme('colors.brand.10');
+  }
+  </style>
--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -1,19 +1,13 @@
 <template>
-  <main class="flex">
-    <Menu />
-    <section class="main w-full">
-      main
-    </section>
-  </main>
+  <Default>
+    Dashboard
+  </Default>
 </template>
 
 <script setup>
-import Menu from '@/components/Menu.vue';
-
+import Default from '@/Layouts/Default.vue';
 </script>
 
 <style lang="scss" scoped> 
-.main {
-  background: theme('colors.brand.10');
-}
+
 </style>
