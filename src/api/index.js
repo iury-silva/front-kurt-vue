901893b8c1adc3373807dc24ba9b3446e42b3672
Helena Della Giustina <haguiar@imply.com>
2024-09-27 22:24:28 -0300

    feat: send the bearer token in the request

--- src/api/index.js
+++ src/api/index.js
@@ -1,10 +1,24 @@
 import axios from "axios";
+import storageController from '@/Controllers/Storage.controller';
 
+
+// Criação da instância da API
 const api = axios.create({
   baseURL: "https://noodletop.com.br/api/kurt/",
   headers: {
     "Content-Type": "application/json",
   },
+});
+
+// Interceptor para adicionar o token Bearer
+api.interceptors.request.use((config) => {
+  const token = storageController.getLocal('token');
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`; 
+  }
+  return config;
+}, (error) => {
+  return Promise.reject(error);
 });
 
 export default api;
