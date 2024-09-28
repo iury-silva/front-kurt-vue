e80c5204a8ae968a24095a84453bf21b142b8ef1
Helena Della Giustina <haguiar@imply.com>
2024-09-27 23:57:55 -0300

    style: Add pages transition

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,51 @@
 <script setup>
+import { computed } from 'vue'; 
 import util from './Controllers/Util.controller';
 import { useToast } from 'primevue/usetoast';
 import Toast from 'primevue/toast';
-import storageController from '@/Controllers/Storage.controller';
 import Loading from './components/Loading.vue';
 import { useGlobalStore } from '@/stores/globais.store'
 
 util.setInitial(useToast())
+
+const loading = computed(() => useGlobalStore().getLoading);
 
 </script>
 
 <template>
   <div>
     <Toast />
-    <RouterView />
-    <Loading :visible="useGlobalStore().getLoading"/>
+    <router-view v-slot="{ Component }">
+      <transition name="fade" mode="out-in">
+        <component :is="Component" />
+      </transition>
+    </router-view>
+    <Loading :visible="loading" />
   </div>
 </template>
 
-<style scoped></style>
+<style scoped>
+.fade-enter-active, .fade-leave-active {
+  transition: opacity 0.3s ease;
+}
+
+.fade-enter {
+  opacity: 0.2;
+}
+
+.fade-enter-active{
+  animation: fadeInOut 1s;
+}
+
+.fade-leave-to {
+  opacity: 0.2;
+}
+@keyframes fadeInOut {
+  0% {
+    opacity: 0.2;
+  } 
+  100% {
+    opacity: 1;
+  }
+}
+</style>
