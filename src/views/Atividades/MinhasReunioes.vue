3206c5f753665afcacb1ea1862e364c33edb1614
Helena Della Giustina <haguiar@imply.com>
2024-10-01 01:50:09 -0300

    fix: fix bug date

--- src/views/Atividades/MinhasReunioes.vue
+++ src/views/Atividades/MinhasReunioes.vue
@@ -91,14 +91,18 @@ const fetchReunioes = async () => {
 };
 
 const formatDate = (dateStr) => {
-    const date = new Date(dateStr);
-    return date.toLocaleDateString('pt-BR', {
-        day: '2-digit',
-        month: '2-digit',
-        year: 'numeric',
-        hour: '2-digit',
-        minute: '2-digit'
-    });
+    if(dateStr){
+        const date = new Date(dateStr);
+        return date.toLocaleDateString('pt-BR', {
+            day: '2-digit',
+            month: '2-digit',
+            year: 'numeric',
+            hour: '2-digit',
+            minute: '2-digit'
+        });
+    } else {
+        return 'Sem data'
+    }
 };
 
 const downloadFile = async (documento) => {
